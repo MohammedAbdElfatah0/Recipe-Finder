158a4d92c79cf3aff9734b6acb40748a85b13cf6
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-03-13 09:49:25 +0200

    core / string constant and style text

---
+++ lib/core/constant/string_manager.dart
@@ -0,0 +1,32 @@
+class StringManager {
+  static final auth = AuthStringManager();
+  static const String appName = 'Recipe Finder';
+}
+
+class AuthStringManager {
+   final String fullName = 'Full Name';
+   final String email = 'Email';
+   final String password = 'Password';
+   final String forgotPassword = 'Forgot Password?';
+   final String login = 'Login';
+   final String createAccount = 'Create Account';
+   final String signUp = 'Sign Up';
+   final String logout = 'Logout';
+   final String apple = 'Apple';
+   final String google = 'Google';
+
+
+final String emailHint = 'Enter your email';
+final String passwordHint = 'Enter your password';
+
+
+   final String welcomeBack = 'Welcome Back';
+   final String alreadyHaveAnAccount = 'Already have an account?';
+   final String dontHaveAnAccount = "Don't have an account?";
+   final String joinCommunity = 'Join our community of food lovers';
+   final String signInToContinue =
+      'Sign in to continue your culinary journey';
+
+   final String orSignUpWith = 'Or sign up with';
+   final String orLoginWith = 'Or continue with';
+}
---
+++ lib/core/constant/style_manager.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class StyleManager {
+  ///size 14
+  static const s14w500 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0,
+    height: 20.0 / 14,
+  );
+  static const s14w600 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0,
+    height: 21.0 / 14,
+  );
+
+  ///size 16
+  static const s16w400 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0,
+    height: 24.0 / 16,
+  );
+  static const s16w600 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0,
+    height: 24.0 / 16,
+  );
+
+  //size 18
+  static const TextStyle s18w700 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+    letterSpacing: -0.27,
+    height: 22.5 / 18,
+  );
+
+  
+  //size 32
+  static const TextStyle s32w700 = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+    letterSpacing: -0.8,
+    height: 40.0 / 32,
+  );
+}
