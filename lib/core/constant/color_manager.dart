a2e72f14461980402997efca5f50b02a11089717
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-03-13 07:50:45 +0200

    add core color manager

---
+++ lib/core/constant/color_manager.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const Color primary = Color(0xFFE53935);
+  static const Color secondary = Color(0xFFFF6E42);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color blackColor = Color(0xFF000000);
+  static const Color greyColor = Color(0xFF9E9E9E);
+  static const Color successColor = Color(0xFF4CAF50);
+  static const Color errorColor = Color(0xFFF44336);
+  static const Color warningColor = Color(0xFFFFC107);
+}
