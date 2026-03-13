23aee05bf647d42d1e33e0342b2935a5199fe0a0
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-03-13 09:48:39 +0200

    add dark gray color

--- lib/core/constant/color_manager.dart
+++ lib/core/constant/color_manager.dart
@@ -6,6 +6,7 @@ class ColorManager {
   static const Color whiteColor = Color(0xFFFFFFFF);
   static const Color blackColor = Color(0xFF000000);
   static const Color greyColor = Color(0xFF9E9E9E);
+  static const Color darkGreyColor = Color(0xFF64748B);
   static const Color successColor = Color(0xFF4CAF50);
   static const Color errorColor = Color(0xFFF44336);
   static const Color warningColor = Color(0xFFFFC107);
