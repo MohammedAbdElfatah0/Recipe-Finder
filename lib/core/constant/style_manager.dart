c3406b0a202b5b523c48f257f043424504351799
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-03-14 06:46:16 +0200

    style - add data type 'TextStyle' - size 11 weight 500 - size 12 weight 600 - size 14 weight 700 - size 20 weight 700

--- lib/core/constant/style_manager.dart
+++ lib/core/constant/style_manager.dart
@@ -1,42 +1,63 @@
 import 'package:flutter/material.dart';
 
 class StyleManager {
+  //size 11
+  static const TextStyle s11w500 = TextStyle(
+    fontSize: 11,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0,
+    height: 0,
+  );
+  //size 12
+  static const TextStyle s12w600 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w700,
+    letterSpacing: 0,
+    height: 16 / 12,
+  );
+
   ///size 14
-  static const s14w400 = TextStyle(
+  static const TextStyle s14w400 = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.w400,
     letterSpacing: 0,
     height: 20.0 / 14,
   );
 
-  static const s14w500 = TextStyle(
+  static const TextStyle s14w500 = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.w500,
     letterSpacing: 0,
     height: 20.0 / 14,
   );
 
-  static const s14w600 = TextStyle(
+  static const TextStyle s14w600 = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.w600,
     letterSpacing: 0,
     height: 21.0 / 14,
   );
+  static const TextStyle s14w700 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    letterSpacing: 0,
+    height: 21.0 / 14,
+  );
 
   ///size 16
-  static const s16w400 = TextStyle(
+  static const TextStyle s16w400 = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w400,
     letterSpacing: 0,
     height: 24.0 / 16,
   );
-  static const s16w600 = TextStyle(
+  static const TextStyle s16w600 = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w600,
     letterSpacing: 0,
     height: 24.0 / 16,
   );
-  static const s16w700 = TextStyle(
+  static const TextStyle s16w700 = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w700,
     letterSpacing: 0,
@@ -48,6 +69,14 @@ class StyleManager {
     fontWeight: FontWeight.w700,
     letterSpacing: -0.27,
     height: 22.5 / 18,
+  );
+
+  //size 20
+  static const TextStyle s20w700 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    letterSpacing: -0.5,
+    height: 28.0 / 20,
   );
 
   //size 32
